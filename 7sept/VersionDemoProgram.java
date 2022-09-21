f250a7d4de6eb2fd69c2e846d499eeabad987e33
SweetyBanerjee98 <82709897+SweetyBanerjee98@users.noreply.github.com>
2022-09-21 13:08:09 +0530

    Add files via upload

---
+++ 7sept/VersionDemoProgram.java
@@ -0,0 +1,26 @@
+/*
+Program :- Write a java Program To print the java version
+@Author :- Sweety Banerjee
+Date:- 16 september 2022
+Timing :- 22:57pm
+
+*/
+
+
+class VersionDemoProgram // Declarig the VersionDemoProgram class
+{ //start this class 
+
+
+public static void main(String[]args)  // Declaring the main method
+
+{ //Main method start
+ 
+   System.out.println("The Version Of java Is : "+ System.getProperty ("java.version")); // printing the java Version on the console
+
+}
+// End of the main Method
+
+
+
+} //end of the VersionDemoProgram class
+
