a70ac24e6f2c4b8370ab28bce81f0b7fdaed6ec6
SweetyBanerjee98 <82709897+SweetyBanerjee98@users.noreply.github.com>
2022-09-25 21:27:04 +0530

    Add files via upload

---
+++ 8sept/IncrementDecrementDemo.java
@@ -0,0 +1,42 @@
+/*
+Program :- Write a java Program to Print (pre_increment ,post_increment, pre_decrement & post_decrement) operator
+@Author :- Sweety Banerjee
+Date:- 8 september 2022
+*/
+
+class IncrementDecrementDemo  //Declaring the IncrementDecrementDemo class
+
+{ //Class start from here
+
+public static void main (String[]args) //Declaring the main Method 
+
+{ // Main Method execution start from here 
+	
+	      //pre_increment ,post_increment Operator
+	
+	    int number1 = 13;  //store the value to the number1 variable with int datatype
+		int number2 = ++number1; //increase the value of a variable by 1 then after value will be generated & store the value to the number2 variable
+		int number3 = number2++; //increase the value of a variable by 1 before the value will be generated & store the value to the number3 variable
+		
+		//printing the value of these variable on the output screen
+		System.out.println("The Number is : " + number2); 
+		System.out.println("The Number is : " + number3);
+		
+		
+		System.out.println();
+		
+		
+		  //pre_decrement & post_decrement Operator
+		  
+		int number4 = 13; //store the value to the number4 variable with int datatype
+		int number5 = --number1;  //decrease the value of a variable by 1 then after value will be generated & store the value to the number2 variable
+		int number6 = number2--; //decrease the value of a variable by 1 before the value will be generated & store the value to the number3 variable
+		
+		
+		//printing the value of these variable on the output screen
+		System.out.println("The Number is : " + number5);
+		System.out.println("The Number is : " + number6);
+	
+} //End of the main method
+
+} //End of the IncrementDecrementDemo class
---
+++ 8sept/LogicalProgramDemo.java
@@ -0,0 +1,30 @@
+/*
+Program :- Write a java Program to print Logical Operations like AND,OR,NOT,XOR
+@Author :- Sweety Banerjee
+#Date :- 8 September 2022
+
+*/
+
+class LogicalProgramDemo // Declaring the LogicalProgramDemo class 
+
+{ //class start from here 
+
+public static void main (String[]args) //Declaring the main method 
+
+{ // Main method execution start
+	
+	boolean a = true; //initializing the status to a variable 
+	boolean b = false; //initializing the status to b variable 
+	
+	System.out.println("The Status is " + (a&b)); //printing the status true or false
+	System.out.println("The Status is " + (a|b)); //printing the status true or false
+	System.out.println("The Status is " + (a^b)); //printing the status true or false
+	System.out.println("The Status is " + ((!a)&(!b))); //printing the status true or false
+	
+	
+	
+	
+} //End of the main method 
+
+
+}  // End of the LogicalProgramDemo class 
