d1d6e3726efeeaa2b7a052582a2af6161861abff
SweetyBanerjee98 <82709897+SweetyBanerjee98@users.noreply.github.com>
2022-09-25 21:20:14 +0530

    Add files via upload

---
+++ 5thsept/FirstProgramDemo.java
@@ -0,0 +1,24 @@
+/*
+Program :- Write a Java Program To Print Hello World
+@Author :- Sweety Banerjee
+Date :- 5 sept 2022
+
+
+*/
+
+class FirstProgramDemo //Declaring the FirstProgramDemo class 
+{ //FirstProgramDemo class start with this Curly braces
+ 
+ 
+ public static void main(String[]args) //Declaring The main Method 
+ 
+ { //starting the Main Method 
+ 
+ 
+	 //System.out.println("Hello World "); //Printing the "Hello World " String on the Output Screen
+	  System.out.println( "The Statement is : " + "Hello World "); //Printing the "Hello World " String on the Output Screen
+	 
+ } //Ending the Main Meyhod
+ 
+
+} //End of the  FirstProgramDemo Class
